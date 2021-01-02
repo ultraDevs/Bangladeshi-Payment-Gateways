2cde0dd39a1091996014abef7d016aea1d435325
mhimon <mahbubhasanimon@gmail.com>
2021-01-02 07:59:09 +0600

    Updated Text Domain

--- bd-payment-gateways.php
+++ bd-payment-gateways.php
@@ -14,7 +14,7 @@
  * Author URI:        https://ultradevs.com
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       bd-payment-gateways
+ * Text Domain:       bangladeshi-payment-gateways
  * Domain Path:       /languages
  */
 
