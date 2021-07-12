b9ccb91afda4b9e89111b396be11e6e365119f1a
mhimon <mahbubhasanimon@gmail.com>
2021-07-12 16:07:36 +0600

    Final Version 2.0.3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'ultradevs/bangladeshi-payment-gateways',
+  ),
+  'versions' => 
+  array (
+    'appsero/client' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'd631da21bccbd0a4ea752cd3b827889883f378af',
+    ),
+    'ultradevs/bangladeshi-payment-gateways' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
