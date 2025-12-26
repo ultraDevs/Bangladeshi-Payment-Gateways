65ebe9893641501c3cb9398210ec4a644bdf765a
mhimon <mahbubhasanimon@gmail.com>
2025-12-26 13:07:13 +0600

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,3 @@
-// const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
-// const path          = require( 'path' );
-
-// module.exports = {
-// 	...defaultConfig,
-// 	entry: {
-// 		'bdpg-blocks': path.resolve( process.cwd(), 'blocks', 'src', 'index.js' ),
-// 	},
-// 	output: {
-// 		...defaultConfig.output,
-// 		path: path.resolve( process.cwd(), 'dist' ),
-// 	},
-// 	externals: {
-// 		'@woocommerce/blocks-registry': 'wc.wcBlocksRegistry',
-// 		'@woocommerce/settings': 'wc.wcSettings',
-// 		'@woocommerce/blocks-components': 'wc.wcBlocksComponents',
-// 		'@woocommerce/base-context/hooks': 'wc.wcBaseContext',
-// 		'@woocommerce/base-context': 'wc.wcBaseContext',
-// 		'@woocommerce/block-data': 'wc.wcBlockData',
-// 	},
-// };
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 const WooCommerceDependencyExtractionWebpackPlugin = require('@woocommerce/dependency-extraction-webpack-plugin');
 const path = require('path');
